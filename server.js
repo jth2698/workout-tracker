ee6c8234b6f7ffccbfc7fbea4122acc652eaf0e2
James Howard <jth2698@gmail.com>
2020-11-07 12:58:41 -0600

    add server.js skeleton

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongojs = require("mongojs");
+
+const app = express();
+
+const databaseUrl = "workout";
+const collections = ["exercises"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/all", (req, res) => {
+    db.workout.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+// Set the app to listen on port 3000
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+});
